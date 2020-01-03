ee2c47b61fc8c595820612e45a5bd62efa84f5ef
Max Hoffmann <Max.Hoffmann@tu-bs.de>
2020-01-03 19:27:14 +0100

    Add webview.preinjection.js (was gitignored before)

---
+++ src/assets/webview/webview.preinjection.js
@@ -0,0 +1,93 @@
+const { ipcRenderer } = require('electron');
+
+var eventShowTab = document.createEvent('event');
+eventShowTab.initEvent('tab-show', true, true)
+
+var eventLeaveTab = document.createEvent('event');
+eventLeaveTab.initEvent('tab-leave', true, true)
+
+var eventPageIsFocus = document.createEvent('event');
+eventPageIsFocus.initEvent('page-focus', true, true)
+
+var eventPageLeaveFocus = document.createEvent('event');
+eventPageLeaveFocus.initEvent('page-leave', true, true)
+
+ipcRenderer.on("webviewdata", (event, webviewData) => {
+    console.log("webviewdata", webviewData);
+    window.scrollTo(webviewData.scrollX, webviewData.scrollY)
+})
+
+ipcRenderer.on("tab-show", (event) => {
+    document.dispatchEvent(eventShowTab)
+})
+ipcRenderer.on("tab-leave", (event) => {
+    document.dispatchEvent(eventLeaveTab)
+})
+ipcRenderer.on("page-focus", (event) => {
+    document.dispatchEvent(eventPageIsFocus)
+})
+ipcRenderer.on("page-leave", (event) => {
+    document.dispatchEvent(eventPageLeaveFocus)
+})
+
+let scroll = {
+    scrollX: window.scrollX,
+    scrollY: window.scrollY
+}
+document.addEventListener("scroll", (event) => {
+    let newScroll = {
+        scrollX: window.scrollX,
+        scrollY: window.scrollY
+    }
+    if (scroll.scrollX != newScroll.scrollX || scroll.scrollY != newScroll.scrollY) {
+        ipcRenderer.sendToHost("change_scroll", {
+            scrollX: window.scrollX,
+            scrollY: window.scrollY
+        })
+        scroll = newScroll
+    }
+})
+
+const availableKeys = [
+    "ArrowRight", "ArrowLeft", "ArrowUp", "ArrowDown"
+]
+function isAvailableKey(key) {
+    return key.length == 1 || _.includes(availableKeys, key)
+}
+/**
+ * Retern key in well structured format
+ * 
+ * @param event the dom event
+ * @param isAllKeyPress if true return on every press except meta keys if false return null on meta keys
+ */
+function keypressEventToTypeInput(event) {
+    if (isAvailableKey(event.key)) {
+        return {
+            key: event.key.toLowerCase(),
+            isAlt: event.altKey,
+            isCtrl: event.ctrlKey,
+            isMeta: event.metaKey,
+            isShift: event.shiftKey
+        }
+    }
+    else return null
+}
+
+document.addEventListener("keydown", (event) => {
+    ipcRenderer.sendToHost("keydown-client", keypressEventToTypeInput(event))
+})
+
+console.log("hallo");
+
+document.addEventListener("tab-show", (event) => {
+    console.log("bingo tab-show");
+})
+document.addEventListener("tab-leave", (event) => {
+    console.log("bingo tab-leave");
+})
+document.addEventListener("page-focus", (event) => {
+    console.log("bingo page-focus");
+})
+document.addEventListener("page-leave", (event) => {
+    console.log("bingo page-leave");
+})
